058a1809e2277f721c743c162980b4347a0500c7
Bikash <magar.30@wright.edu>
2025-08-21 21:45:56 -0400

    asdfasdf

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("hello world")
 print("my name is Shirish")
  git config --global user.email "magar.30@wright.edu"
-  git config --global user.name "Bikash"
+  git config --global user.name "Bikash"ewr5rtqweerqw
